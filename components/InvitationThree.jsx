020fdaee6b0880039b5ebc5f0da3e37dee70da88
Your Name <anjalisuthar114@gmail.com>
2026-08-01 13:40:05 +0530

    image place

--- components/InvitationThree.jsx
+++ components/InvitationThree.jsx
@@ -4,7 +4,7 @@ const InvitationThree = () => {
   return (
     <div className="w-full flex items-center justify-center  p-4">
       <img
-        src="/assets/Punamiya/Inner Invite-01.png"
+        src="/Inner_Invite-01.png"
         alt="Invitation"
         className="max-w-full max-h-screen object-cover"
       />
